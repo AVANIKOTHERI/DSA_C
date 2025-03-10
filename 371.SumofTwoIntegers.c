6357b0d7b086dc18f139b7af8178e4933c805423
AVANIKOTHERI <aavanikotheri@gmail.com>
2025-03-10 16:18:12 +0530

    Update 371.SumofTwoIntegers.c

--- 371.SumofTwoIntegers.c
+++ 371.SumofTwoIntegers.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
 
 int getSum(int a, int b) {
+
     while (b != 0) {
         int sum = a ^ b;       // XOR: Sum without carry
-        int carry = (a & b) << 1;  // AND: Carry shifted left
+        unsigned int carry = ((unsigned int)(a & b)) << 1;
         
         a = sum;   // Update a to sum
         b = carry; // Update b to carry
     }
     return a;
 }
+
 
 int main() {
     int a = 1, b = 2;
